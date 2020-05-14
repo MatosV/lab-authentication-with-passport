fd32ad81893d1cff4765467619e9cfbfa2435dd7
Vasco Matos <vascomato@gmail.com>
2020-05-14 15:18:05 +0100

    .

--- configure-passport.js
+++ configure-passport.js
@@ -10,17 +10,26 @@ const PassportLocalStrategy = passportLocal.Strategy;
 const bcrypt = require('bcrypt');
 const User = require('./models/user');
 
-
 passport.serializeUser((user, callback) => {
   callback(null, user._id);
 });
 
 passport.deserializeUser((id, callback) => {
   User.findById(id)
-    .then(user => {
+    .then((user) => {
       callback(null, user);
     })
-    .catch(error => {
+    .catch((error) => {
+      callback(error);
+    });
+});
+
+passport.deserializeUser((id, callback) => {
+  User.findById(id)
+    .then((user) => {
+      callback(null, user);
+    })
+    .catch((error) => {
       callback(error);
     });
 });
@@ -30,28 +39,19 @@ passport.use(
   new PassportLocalStrategy({}, (username, password, callback) => {
     // Perform your authentication logic and call the callback function,
     // passing it null in the first parameter and the user document in the second
-    let user;
-    User.findOne({
-      username
-    })
-    .then(document => {
-      user = document;
-      if (document) {
-        return bcrypt.compare(password, user.passwordHash)
-      } else {
-        return Promise.reject(new Error('User not valid'));
-      }
-    })
-    .then(pw => {
-      if (pw) {
+    bcrypt
+      .hash(password, 15)
+      .then((hashandsalt) => {
+        return User.create({
+          username,
+          passwordHash: hashandsalt
+        });
+      })
+      .then((user) => {
         callback(null, user);
-      } else {
-        return Promise.reject(new Error('Wrong Password'));
-      }
-    })
-    .catch(error => {
-      callback(error)
-    })
+      })
+      .catch((error) => {
+        callback(error);
+      });
   })
 );
-
